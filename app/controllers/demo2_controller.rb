c38a7f998f55a948ab29087bcd85efb1dc0af13b
junjun <buptjunjun@163.com>
2014-07-02 15:53:38 +0800

    second version

--- app/controllers/demo2_controller.rb
+++ app/controllers/demo2_controller.rb
@@ -8,15 +8,23 @@ class Demo2Controller < ApplicationController
 
         if @q_type == 0
            text = @question.text
-           texts = text.split("||")
-           render :template => "demo2/index",locals: {:teststr=>'test',:q=>@question,:texts => texts}
+           texts = text.split("|||")
+           chineses = texts[0].split("|")
+           englishes = texts[1].split("|")
+           words = []
+           chineses.each_with_index { |c, index|
+                e = englishes[index]
+                word = {"c"=>c,"e"=>e}
+                words.append(word)
+           }
+           render :template => "demo2/index",locals: {:teststr=>'test', :q=>@question, :texts => texts, :words=>words}
         elsif  @q_type == 1
             answers = @question.answers
             render :template => "demo2/question2", locals: { :topic=>@topic,:question=>@question,:answers=>answers}
         elsif @q_type == 2
           answers = @question.answers
           images = answers[0].text.split(",")
-          render :template => "demo2/question3", locals: {:images=>images}
+          render :template => "demo2/question3", locals: {:images=>images,:question=>@question}
         else
           redirect_to ("/demo2/index?t_id=#{@topic.id}&q_id=0&q_type=0")
         end
