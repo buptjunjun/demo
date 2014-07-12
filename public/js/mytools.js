34a01c5f2549fb0ad5c3df040778887841cefa9c
junjun <Admin@ninja-2.local>
2014-07-12 14:01:06 +0800

    change freda's style

---
+++ public/js/mytools.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Admin on 14-7-12.
+ */
+
+
+/**
+ *  usage: set class to tooltip
+ */
+    $(function(){
+        var x = 10;
+        var y = 20;
+        $(".mytips").on("mouseover",function(e){
+            this.myTitle = this.title;
+            this.title = "";
+            var tooltip = "<div id='tooltip'>"+ this.myTitle +"<\/div>"; //创建 div 元素 文字提示
+            $("body").append(tooltip);    //把它追加到文档中
+            $("#tooltip")
+                .css({
+                    "top": (e.pageY+y) + "px",
+                    "left": (e.pageX+x)  + "px"
+                }).show("fast");      //设置x坐标和y坐标，并且显示
+        }).on("mouseout",function(){
+            this.title = this.myTitle;
+            $("#tooltip").remove();   //移除
+        }).on("mousemove",function(e){
+            $("#tooltip")
+                .css({
+                    "top": (e.pageY+y) + "px",
+                    "left": (e.pageX+x)  + "px"
+                });
+        });
+    })
