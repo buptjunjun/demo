86145d05acaa52f02fd5dc86348856238189b2cb
buptjunjun <buptjunjun@163.com>
2014-07-12 22:27:08 +0800

    add preload

--- public/js/mytools.js
+++ public/js/mytools.js
@@ -35,3 +35,12 @@
     $(function(){
         loadtips();
     })
+
+    function preloadimages(arr){
+        var newimages=[]
+        var arr=(typeof arr!="object")? [arr] : arr  //确保参数总是数组
+        for (var i=0; i<arr.length; i++){
+            newimages[i]=new Image()
+            newimages[i].src=arr[i]
+        }
+    }
